7c60090f44cfd2f7ab123784b0c84cdfb11f3194
Osvaldo David Huerta Padilla <1719110580@utectulancingo.edu.mx>
2020-05-24 03:33:27 +0000

    Programa Creación_clases_objetos funcionando

---
+++ Semana_2/Creacion_clases_objetos.py
@@ -0,0 +1,513 @@
+class Banco:
+
+    #Atributos
+    Puertas = "Cristal"
+    Paredes ="Lisas"
+    Cajeros = "Electronicos"
+    Personal_ventanilla = "Trabajadores"
+    Asientos_espera = "Metal"
+    Estacionamiento = "Grande"
+    Clientes = "Personas en general"
+    Servicios = "Retiros"
+    Pantallas = "Pantallas planas"
+    Sistema_organizacion = "Numeración"
+
+    #Métodos
+    def pagar(self):
+        print("Pagar")
+
+    def recibir(self):
+        print("Recibir")
+
+    def contar(self):
+        print("Contar")
+
+    def financiar(self):
+        print("Financiar")
+
+    def __init__(self):
+        print("Metodos y atributos de un banco")
+
+objeto_banco = Banco()
+objeto_banco.pagar()
+objeto_banco.recibir()
+objeto_banco.contar()
+objeto_banco.financiar()
+print(objeto_banco.Puertas)
+print(objeto_banco.Paredes)
+print(objeto_banco.Cajeros)
+print(objeto_banco.Personal_ventanilla)
+print(objeto_banco.Asientos_espera)
+print(objeto_banco.Estacionamiento)
+print(objeto_banco.Clientes)
+print(objeto_banco.Servicios)
+print(objeto_banco.Pantallas)
+print(objeto_banco.Sistema_organizacion)
+print("_____________________________________")
+class Estudiante:
+    # Atributos
+    Uniforme = "Azul"
+    Credencial = "Papel"
+    Mochila = "Escolar"
+    Libros =" De sus asignaturas"
+    Lapiceros = "De colores"
+    Peinado = "Normal"
+    Hojas = "De colores o blancas"
+    Apuntes = "Escritos"
+    Lentes = "Normales"
+    Cuerpo = "Delgado"
+
+    #Metodos
+
+    def estudiar(self):
+        print("Estudiar")
+
+    def aprender(self):
+        print("Aprender")
+
+    def participar(self):
+        print("Participar")
+
+    def cuestionar(self):
+        print("Cuestionar")
+
+    def investigar(self):
+        print("Investigar")
+
+    def __init__(self):
+        print("Metodos y atributos de un estudiante")
+
+objeto_estudiante = Estudiante()
+objeto_estudiante.estudiar()
+objeto_estudiante.aprender()
+objeto_estudiante.participar()
+objeto_estudiante.cuestionar()
+objeto_estudiante.investigar()
+
+print(objeto_estudiante.Uniforme)
+print(objeto_estudiante.Credencial)
+print(objeto_estudiante.Mochila)
+print(objeto_estudiante.Libros)
+print(objeto_estudiante.Lapiceros)
+print(objeto_estudiante.Peinado)
+print(objeto_estudiante.Hojas)
+print(objeto_estudiante.Apuntes)
+print(objeto_estudiante.Lentes)
+print(objeto_estudiante.Cuerpo)
+("_____________________________________")
+class Avion:
+    # Atributos
+    Helices = "Planas"
+    Turbinas = "Turbojet"
+    Motor = "Electrico"
+    Asientos = "Comodos"
+    Puertas = "Altas"
+    Piloto = "Experto"
+    Controles = "Aptos"
+    Ventanas = "Redondas"
+    Pasajeros = "Personas normales"
+    Maletero = "Ancho"
+
+    #Metodos
+
+    def volar(self):
+        print("Volar")
+
+    def aterrizar(self):
+        print("aterrizar")
+
+    def transportar(self):
+        print("Transportar")
+
+    def informar(self):
+        print("Informar")
+
+    def proteger(self):
+        print("Proteger")
+
+    def __init__(self):
+        print("Metodos y atributos de un avion")
+
+objeto_avion = Avion()
+objeto_avion.volar()
+objeto_avion.aterrizar()
+objeto_avion.transportar()
+objeto_avion.informar()
+objeto_avion.proteger
+
+print(objeto_avion.Helices)
+print(objeto_avion.Turbinas)
+print(objeto_avion.Motor)
+print(objeto_avion.Asientos)
+print(objeto_avion.Puertas)
+print(objeto_avion.Piloto)
+print(objeto_avion.Controles)
+print(objeto_avion.Ventanas)
+print(objeto_avion.Pasajeros)
+print(objeto_avion.Maletero)
+
+print("_______________________________________")
+class Vacaciones:
+    #Atributos
+    Tiempo = "7 dias"
+    Dinero = "$10,000 pesos"
+    Personas = "Familia y amigos"
+    Clima = "Soleado"
+    Camino = "Largo"
+    Fenomenos = "Tsunamis"
+    Hoteles = "5 estrellas"
+    Atracciones = "Feria"
+    Ambiente = "Familiar"
+    Familia = "Hijos y esposa"
+
+    #Metodos
+    def tranquilizar(self):
+        print("Tranquilizar")
+
+    def disfrutar(self):
+        print("Disfrutar")
+
+    def entretener(self):
+        print("Entretener")
+
+    def relajar(self):
+        print("Relajar")
+
+    def sentir(self):
+        print("sentir")
+
+    def __init__(self):
+        print("Metodos y atributos de unas vacaciones")
+
+objeto_vacaciones = Vacaciones()
+objeto_vacaciones.tranquilizar()
+objeto_vacaciones.disfrutar()
+objeto_vacaciones.entretener()
+objeto_vacaciones.relajar()
+objeto_vacaciones.sentir()
+
+print(objeto_vacaciones.Tiempo)
+print(objeto_vacaciones.Dinero)
+print(objeto_vacaciones.Personas)
+print(objeto_vacaciones.Clima)
+print(objeto_vacaciones.Camino)
+print(objeto_vacaciones.Fenomenos)
+print(objeto_vacaciones.Hoteles)
+print(objeto_vacaciones.Atracciones)
+print(objeto_vacaciones.Ambiente)
+print(objeto_vacaciones.Familia)
+
+print("_______________________________________")
+class Series:
+
+    #Atributos
+    Terror = "Zombies"
+    Comedia = "Chistes"
+    Drama = "Telenovelas"
+    Caricatura = "Bob esponja"
+    Accion = "Robos"
+    Anime = "Videojuegos"
+    Adolescente = "Historias de amor"
+    Misterio = "Detectives"
+    Suspenso = "Historias reales"
+    Infantil = "Programas educativos"
+
+    #Metodos
+    def entretener(self):
+        print("Entretener")
+
+    def distraer(self):
+        print("Distraer")
+
+    def convencer(self):
+        print("Convencer")
+
+    def informar(self):
+        print("informar")
+
+    def relajar(self):
+        print("Relajar")
+
+    def __init__(self):
+        print("Metodos y atributos de Series de TV")
+
+objeto_series = Series()
+objeto_series.entretener()
+objeto_series.distraer()
+objeto_series.convencer()
+objeto_series.informar()
+objeto_series.relajar()
+
+print(objeto_series.Terror)
+print(objeto_series.Comedia)
+print(objeto_series.Drama)
+print(objeto_series.Caricatura)
+print(objeto_series.Accion)
+print(objeto_series.Anime)
+print(objeto_series.Adolescente)
+print(objeto_series.Misterio)
+print(objeto_series.Suspenso)
+print(objeto_series.Infantil)
+
+print("____________________________________")
+#_____________________________________________
+class Futbolistas:
+    #Atributos
+    Uniforme = "Rojo"
+    Tenis = "Gruesos"
+    Piernas = "Fuertes"
+    Balon = "Profesional"
+    Cabeza = "Fuerte"
+    Pecho = "Fuerte"
+    Casaca = "Tela nylon"
+    Numero = "10"
+    Nombre = "Messi"
+    Ficha = "Con foto"
+
+    #Metodos
+
+    def patear(self):
+        print("Patear")
+
+    def anotar(self):
+        print("Anotar")
+
+    def pasar(self):
+        print("pasar")
+
+    def tirar(self):
+        print("tirar")
+
+    def correr(self):
+        print("correr")
+
+    def __init__(self):
+        print("Metodos y atributos de Futbolistas")
+
+objeto_futbolistas = Futbolistas()
+objeto_futbolistas.patear()
+objeto_futbolistas.anotar()
+objeto_futbolistas.pasar()
+objeto_futbolistas.tirar()
+objeto_futbolistas.correr()
+
+print(objeto_futbolistas.Uniforme)
+print(objeto_futbolistas.Tenis)
+print(objeto_futbolistas.Piernas)
+print(objeto_futbolistas.Balon)
+print(objeto_futbolistas.Cabeza)
+print(objeto_futbolistas.Pecho)
+print(objeto_futbolistas.Casaca)
+print(objeto_futbolistas.Numero)
+print(objeto_futbolistas.Nombre)
+print(objeto_futbolistas.Ficha)
+
+print("________________________________________")
+
+class Coche:
+    #Atributos
+    Puertas = "4"
+    Llantas = "Grandes"
+    Techo = "Alto"
+    Motor = "Hibrido"
+    Ventanas = "Automaticas"
+    Parabrisas = "Amplio"
+    Asientos = "Comodos"
+    Volante = "Redondo"
+    Guantera = "Pequeña"
+    Cajuela = "Amplia"
+
+    #Metodos
+    def conducir(self):
+        print("Conducir")
+
+    def transportar(self):
+        print("transportar")
+
+    def proteger(self):
+        print("proteger")
+
+    def facilitar(self):
+        print("facilitar")
+
+    def viajar(self):
+        print("viajar")
+
+    def __init__(self):
+        print("Metodos y atributos de un coche")
+
+objetos_coche = Coche()
+objetos_coche.conducir()
+objetos_coche.transportar()
+objetos_coche.proteger()
+objetos_coche.facilitar()
+objetos_coche.viajar()
+
+print(objetos_coche.Puertas)
+print(objetos_coche.Llantas)
+print(objetos_coche.Techo)
+print(objetos_coche.Motor)
+print(objetos_coche.Ventanas)
+print(objetos_coche.Parabrisas)
+print(objetos_coche.Asientos)
+print(objetos_coche.Volante)
+print(objetos_coche.Guantera)
+print(objetos_coche.Cajuela)
+
+print("_________________________________")
+#_______________________________
+class Calculadora:
+    #Atributos
+    Botones = "Pequeños"
+    Pantalla = "Rectangular"
+    Plastico = "Resistente"
+    Baterias = "Redondas"
+    Cables = "Conectores"
+    Sistema = "Matematico"
+    Energia = "Electrica"
+    Diseño = "Simple"
+    Forma = "Rectangular"
+    Operadores = "Matematicos"
+
+    #Metodos
+    def operar(self):
+        print("Operar")
+
+    def resolver(self):
+        print("Resolver")
+
+    def borrar(self):
+        print("Borrar")
+
+    def resultar(self):
+        print("Resultar")
+
+    def introducir(self):
+        print("Introducir")
+
+    def __init__(self):
+        print("Metodos y atributos de una calculadora")
+
+objeto_calculadora = Calculadora(
+)
+objeto_calculadora.operar()
+objeto_calculadora.resolver()
+objeto_calculadora.borrar()
+objeto_calculadora.resultar()
+objeto_calculadora.introducir()
+
+print(objeto_calculadora.Botones)
+print(objeto_calculadora.Pantalla)
+print(objeto_calculadora.Plastico)
+print(objeto_calculadora.Baterias)
+print(objeto_calculadora.Cables)
+print(objeto_calculadora.Sistema)
+print(objeto_calculadora.Energia)
+print(objeto_calculadora.Diseño)
+print(objeto_calculadora.Forma)
+print(objeto_calculadora.Operadores)
+
+print("_________________________________________")
+#_______________________________________
+class Cajero_automatico:
+
+    #Atributos
+    Pantalla = "Tactil"
+    Botones = "Verdes"
+    Sistema = "De pago"
+    Dinero = "Billetes"
+    Papel_ticket = "Impreso"
+    Caja = "Organizada"
+    Lector = "Digital"
+    Logo = "De la sucursal"
+    Ranuras = "Para tarjeta"
+    Microfono = "Para hablar"
+
+    #Metodos
+    def pagar(self):
+        print("Pagar")
+
+    def recibir(self):
+        print("Recibir")
+
+    def devolver(self):
+        print("Devolver")
+
+    def cambiar(self):
+        print("Cambiar")
+
+    def introducir(self):
+        print("Introducir")
+
+    def __init__(self):
+        print("Metodos y atributos de un cajero automatico")
+
+objeto_cajero = Cajero_automatico()
+objeto_cajero.pagar()
+objeto_cajero.recibir()
+objeto_cajero.devolver()
+objeto_cajero.cambiar()
+objeto_cajero.introducir()
+
+print(objeto_cajero.Pantalla)
+print(objeto_cajero.Botones)
+print(objeto_cajero.Sistema)
+print(objeto_cajero.Dinero)
+print(objeto_cajero.Papel_ticket)
+print(objeto_cajero.Caja)
+print(objeto_cajero.Lector)
+print(objeto_cajero.Logo)
+print(objeto_cajero.Ranuras)
+print(objeto_cajero.Microfono)
+
+print("___________________________________________")
+#_______________________________________
+class Google_classroom:
+    #Atributos
+    Sistema = "Nube"
+    Clases = "No presenciales"
+    Profesores = "Docentes de la institucion educativa"
+    Datos = "Fechas y detalles de trabajos"
+    Configuraciones = "Recibir notificaciones"
+    Logo = "Google classroom"
+    Fechas = "Fechas de entrega de trabajos"
+    Metodos = "Subir archivos y/o enlaces"
+    Puntuacion = "10/10"
+    Notas = "Comentarios"
+
+    #Metodos
+    def indicar(self):
+        print("Indicar")
+
+    def subir(self):
+        print("Subir")
+
+    def permitir(self):
+        print("Permitir")
+
+    def configurar(self):
+        print("configurar")
+
+    def agregar(self):
+        print("Agregar")
+
+    def __init__(self):
+        print("Metodos y atributps de Google classroom")
+
+objeto_Google_classroom = Google_classroom()
+objeto_Google_classroom.indicar()
+objeto_Google_classroom.subir()
+objeto_Google_classroom.permitir
+objeto_Google_classroom.configurar()
+objeto_Google_classroom.agregar()
+
+print(objeto_Google_classroom.Sistema)
+print(objeto_Google_classroom.Clases)
+print(objeto_Google_classroom.Profesores)
+print(objeto_Google_classroom.Datos)
+print(objeto_Google_classroom.Configuraciones)
+print(objeto_Google_classroom.Logo)
+print(objeto_Google_classroom.Fechas)
+print(objeto_Google_classroom.Metodos)
+print(objeto_Google_classroom.Puntuacion)
+print(objeto_Google_classroom.Notas)
